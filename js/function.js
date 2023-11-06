c5ff299e24e09cd845e22774eb2ce3ecaadce21f
TesterJo <testerjhy@gmail.com>
2023-11-06 10:18:36 +0900

    20231106_update

--- js/function.js
+++ js/function.js
@@ -1,30 +1,36 @@
-// 朝の挨拶を出力する関数
-const sayGoodMorning = () => {
-  console.log("Good Morning");
-};
+// // 朝の挨拶を出力する関数
+// const sayGoodMorning = () => {
+//   console.log("Good Morning");
+// };
 
-// 夜の挨拶を出力する関数
-const sayGoodEvening = () => {
-  console.log("Good Evening");
-};
+// // 夜の挨拶を出力する関数
+// const sayGoodEvening = () => {
+//   console.log("Good Evening");
+// };
 
-sayGoodMorning();
-sayGoodEvening();
+// sayGoodMorning();
+// sayGoodEvening();
 
-const calculateTotal = (price) => {
-  console.log(price + 500 + "円");
-};
+// const calculateTotal = (price) => {
+//   console.log(price + 500 + "円");
+// };
 
-calculateTotal(1200);
+// calculateTotal(1200);
 
-const addTwoArguments = (price, shippingFee) => {
-  console.log(price + shippingFee + "円");
-};
+// const addTwoArguments = (price, shippingFee) => {
+//   console.log(price + shippingFee + "円");
+// };
 
-addTwoArguments(1200, 500);
+// addTwoArguments(1200, 500);
 
-const double = (num) => {
-  return num * 2;
-};
+// const double = (num) => {
+//   return num * 2;
+// };
 
-console.log(double(30));
+// console.log(double(30));
+
+let name = "Sara";
+
+function getUserName(name, age) {
+  let realName = name;
+}
--- js/loop.js
+++ js/loop.js
@@ -2,12 +2,15 @@ let num = Math.floor(Math.random() * 5);
 
 console.log("最小の値は" + num + "です");
 
+/*
 while (num !== 0) {
   num = Math.floor(Math.random() * 5);
 
   console.log("現在の値は" + num + "です");
 }
+*/
 
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
+// 1 + 2 + 3 ... + 99 + 100
+for (let i = 1; i <= 100; i += 1) {
+  // 処理を書く。
 }
